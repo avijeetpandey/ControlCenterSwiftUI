4be8d113b3ef9a77c65776c65aaa434743ea0bfe
avijeet <avijeetpandey25@gmail.com>
2024-06-23 01:35:00 +0530

    Initial Commit

---
+++ ControlCenterSwiftUI/ControlCenterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlCenterSwiftUIApp.swift
+//  ControlCenterSwiftUI
+//
+//  Created by Avijeet on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ControlCenterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
